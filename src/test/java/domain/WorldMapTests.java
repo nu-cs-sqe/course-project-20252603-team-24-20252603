7ed9e103a271337dbf867be7397484e9a53f42d9
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 17:29:24 -0500

    GetArmies_TerritoryWithFiveArmies_ReturnsFive passes

--- src/test/java/domain/WorldMapTests.java
+++ src/test/java/domain/WorldMapTests.java
@@ -147,4 +147,17 @@ public class WorldMapTests {
 
         assertEquals(1, map.getArmies(TerritoryName.ALASKA));
     }
+
+    @Test
+    public void GetArmies_TerritoryWithFiveArmies_ReturnsFive() {
+        Map<TerritoryName, Territory> territories = createTerritories();
+        Map<TerritoryName, Set<TerritoryName>> neighbors = createNeighbors();
+        Territory mockAlaska = EasyMock.createMock(Territory.class);
+        EasyMock.expect(mockAlaska.getArmies()).andStubReturn(5);
+        EasyMock.replay(mockAlaska);
+        territories.put(TerritoryName.ALASKA, mockAlaska);
+        WorldMap map = new WorldMap(territories, neighbors);
+
+        assertEquals(5, map.getArmies(TerritoryName.ALASKA));
+    }
 }
