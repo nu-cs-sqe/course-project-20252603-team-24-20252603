fdd74556e4e0448320613c9788c71e521373f826
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 17:25:31 -0500

    AreNeighbors_AlaskaAndAlberta_ReturnsTrue passes

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // Source: https://mvnrepository.com/artifact/org.easymock/easymock
+    testImplementation("org.easymock:easymock:5.4.0")
 }
 
 java {
