57071ca8dd360407d6111897110a51a215a58981
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 17:25:31 -0500

    AreNeighbors_AlaskaAndAlberta_ReturnsTrue passes

---
+++ src/main/java/domain/WorldMap.java
@@ -0,0 +1,38 @@
+package domain;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class WorldMap {
+    private final Map<TerritoryName, Territory> territories;
+    private final Map<TerritoryName, Set<TerritoryName>> neighbors;
+
+    WorldMap() {
+        this.territories = new HashMap<>();
+        this.neighbors = new HashMap<>();
+        initializeTerritories();
+        initializeNeighbors();
+    }
+
+    WorldMap(Map<TerritoryName, Territory> territories, Map<TerritoryName, Set<TerritoryName>> neighbors) {
+        this.territories = territories;
+        this.neighbors = neighbors;
+    }
+
+    boolean areNeighbors(TerritoryName first, TerritoryName second) {
+        return neighbors.containsKey(first) &&
+                neighbors.get(first).contains(second);
+    }
+
+    private void initializeTerritories() {
+        for (TerritoryName name : TerritoryName.values()) {
+            territories.put(name, new Territory(name));
+            neighbors.put(name, new HashSet<>());
+        }
+    }
+
+    private void initializeNeighbors() {
+    }
+}
