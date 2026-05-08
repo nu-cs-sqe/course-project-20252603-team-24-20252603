aa435e04fa301790fd9f0e04ccbe8f01fb3563b7
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 16:08:17 -0500

    GetArmies_NewTerritory_ReturnsZero passes

--- src/main/java/domain/Territory.java
+++ src/main/java/domain/Territory.java
@@ -2,12 +2,18 @@ package domain;
 
 class Territory {
     private final TerritoryName name;
+    private int armies;
 
     Territory(TerritoryName name) {
         this.name = name;
+        this.armies = 0;
     }
 
     TerritoryName getName() {
         return name;
+    }
+
+    int getArmies() {
+        return armies;
     }
 }
