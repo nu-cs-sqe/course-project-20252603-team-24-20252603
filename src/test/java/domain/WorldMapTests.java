2e8f81d3958caa0b71563df20a7fb950b58bc265
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 17:25:31 -0500

    AreNeighbors_AlaskaAndAlberta_ReturnsTrue passes

---
+++ src/test/java/domain/WorldMapTests.java
@@ -0,0 +1,41 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+import org.easymock.EasyMock;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorldMapTests {
+
+    private Map<TerritoryName, Territory> createTerritories() {
+        Map<TerritoryName, Territory> territories = new HashMap<>();
+        for (TerritoryName name : TerritoryName.values()) {
+            Territory mock = EasyMock.createMock(Territory.class);
+            EasyMock.replay(mock);
+            territories.put(name, mock);
+        }
+        return territories;
+    }
+
+    private Map<TerritoryName, Set<TerritoryName>> createNeighbors() {
+        Map<TerritoryName, Set<TerritoryName>> neighbors = new HashMap<>();
+        for (TerritoryName name : TerritoryName.values()) {
+            neighbors.put(name, new HashSet<>());
+        }
+        return neighbors;
+    }
+
+    @Test
+    public void AreNeighbors_AlaskaAndAlberta_ReturnsTrue() {
+        Map<TerritoryName, Territory> territories = createTerritories();
+        Map<TerritoryName, Set<TerritoryName>> neighbors = createNeighbors();
+        neighbors.get(TerritoryName.ALASKA).add(TerritoryName.ALBERTA);
+        neighbors.get(TerritoryName.ALBERTA).add(TerritoryName.ALASKA);
+        WorldMap map = new WorldMap(territories, neighbors);
+
+        assertTrue(map.areNeighbors(TerritoryName.ALASKA, TerritoryName.ALBERTA));
+    }
+}
