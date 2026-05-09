09766d796558bbc58158ce1e0b37134fa71ca831
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 21:23:34 -0500

    Constructor_FivePlayers_PhaseIsScramble passes

--- src/test/java/domain/RiskGameTests.java
+++ src/test/java/domain/RiskGameTests.java
@@ -42,4 +42,17 @@ public class RiskGameTests {
         RiskGame game = new RiskGame(players, stubbedRandom(0));
         assertEquals(GamePhase.SCRAMBLE, game.getPhase());
     }
+
+    @Test
+    public void Constructor_FivePlayers_PhaseIsScramble() {
+        Map<PlayerColor, String> players = Map.of(
+                PlayerColor.RED, "Jovy",
+                PlayerColor.BLUE, "Justin",
+                PlayerColor.GREEN, "Prashant",
+                PlayerColor.ORANGE, "David",
+                PlayerColor.PINK, "Jonathan"
+        );
+        RiskGame game = new RiskGame(players, stubbedRandom(0));
+        assertEquals(GamePhase.SCRAMBLE, game.getPhase());
+    }
 }
