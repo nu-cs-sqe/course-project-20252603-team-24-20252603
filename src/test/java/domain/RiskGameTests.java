a7d0705ab74f21a43f0b209bb0cbfd11ccffc462
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-08 22:16:27 -0500

    GetArmiesToPlace_AfterClaimingOneTerritory_Returns34 fix

--- src/test/java/domain/RiskGameTests.java
+++ src/test/java/domain/RiskGameTests.java
@@ -211,8 +211,9 @@ public class RiskGameTests {
         RiskGame game = new RiskGame(threePlayerMap(), stubbedRandom(0));
         game.setCurrentPlayer(PlayerColor.RED);
         WorldMap mockMap = EasyMock.createMock(WorldMap.class);
-        EasyMock.expect(mockMap.isUnclaimed(EasyMock.anyObject())).andStubReturn(true);
         mockMap.claim(EasyMock.anyObject(), EasyMock.anyObject());
+        EasyMock.expectLastCall();
+        mockMap.addArmies(EasyMock.anyObject(), EasyMock.anyInt());
         EasyMock.expectLastCall();
         EasyMock.replay(mockMap);
         game.provideWorldMap(mockMap);
