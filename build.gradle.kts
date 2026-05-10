7dc33444f8fe42a4499a0dc0071558559a243e60
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-10 14:04:56 -0500

    add jacoco and pitest

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     id("org.openjfx.javafxplugin") version "0.1.0"
     checkstyle
     id("com.github.spotbugs") version "6.0.25"
+    jacoco
+    id("info.solidsoft.pitest") version "1.15.0"
 }
 
 group = "nu.csse.sqe"
@@ -31,10 +33,6 @@ java {
 
 tasks.compileJava {
     options.release = 11
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 application {
@@ -80,4 +78,34 @@ tasks.spotbugsMain {
         outputLocation = layout.buildDirectory.file("reports/spotbugs/spotbugs.html")
         setStylesheet("fancy-hist.xsl")
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("reports/jacoco")
+    }
+}
+
+pitest {
+    targetClasses = setOf("domain.*")
+    targetTests = setOf("domain.*")
+    junit5PluginVersion = "1.2.1"
+    pitestVersion = "1.15.0"
+    threads = 4
+    outputFormats = setOf("HTML")
+    timestampedReports = false
+    testSourceSets.set(listOf(sourceSets.test.get()))
+    mainSourceSets.set(listOf(sourceSets.main.get()))
+    jvmArgs.set(listOf("-Xmx1024m"))
+    useClasspathFile.set(true)
+    fileExtensionsToFilter.addAll("xml")
+    exportLineCoverage = true
 }
