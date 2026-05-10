6f6cab44d7219b4ab592ae428935e79e10e71613
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-05-10 13:13:47 -0500

    refactored codebase for spotbugs and checkstyle

--- src/main/java/domain/WorldMap.java
+++ src/main/java/domain/WorldMap.java
@@ -16,14 +16,15 @@ class WorldMap {
         initializeNeighbors();
     }
 
-    WorldMap(Map<TerritoryName, Territory> territories, Map<TerritoryName, Set<TerritoryName>> neighbors) {
+    WorldMap(Map<TerritoryName, Territory> territories, Map<TerritoryName,
+            Set<TerritoryName>> neighbors) {
         this.territories = territories;
         this.neighbors = neighbors;
     }
 
     boolean areNeighbors(TerritoryName first, TerritoryName second) {
-        return neighbors.containsKey(first) &&
-                neighbors.get(first).contains(second);
+        return neighbors.containsKey(first)
+                && neighbors.get(first).contains(second);
     }
 
     private void initializeTerritories() {
