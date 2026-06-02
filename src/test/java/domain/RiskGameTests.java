31954bf9a7520940f0f2de1c640203f7132cf72f
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-06-01 20:06:14 -0500

    Fortify_AfterEndTurn_NewPlayerCanFortify passes

--- src/test/java/domain/RiskGameTests.java
+++ src/test/java/domain/RiskGameTests.java
@@ -1088,4 +1088,32 @@ public class RiskGameTests {
                 () -> game.fortify(TerritoryName.ALASKA, TerritoryName.ALBERTA, 1));
         EasyMock.verify(mockMap);
     }
+
+    @Test
+    public void Fortify_AfterEndTurn_NewPlayerCanFortify() {
+        RiskGame game = new RiskGame(threePlayerMap(), stubbedRandom(0));
+        WorldMap mockMap = EasyMock.createMock(WorldMap.class);
+        EasyMock.expect(mockMap.isOwnedBy(TerritoryName.ALASKA, PlayerColor.RED)).andStubReturn(true);
+        EasyMock.expect(mockMap.isOwnedBy(TerritoryName.ALBERTA, PlayerColor.RED)).andStubReturn(true);
+        EasyMock.expect(mockMap.areNeighbors(TerritoryName.ALASKA, TerritoryName.ALBERTA)).andStubReturn(true);
+        EasyMock.expect(mockMap.getArmies(TerritoryName.ALASKA)).andStubReturn(3);
+        EasyMock.expect(mockMap.isOwnedBy(TerritoryName.ALASKA, PlayerColor.BLUE)).andStubReturn(true);
+        EasyMock.expect(mockMap.isOwnedBy(TerritoryName.ALBERTA, PlayerColor.BLUE)).andStubReturn(true);
+        EasyMock.expect(mockMap.areNeighbors(TerritoryName.ALASKA, TerritoryName.ALBERTA)).andStubReturn(true);
+        EasyMock.expect(mockMap.getArmies(TerritoryName.ALASKA)).andStubReturn(3);
+        mockMap.removeArmies(TerritoryName.ALASKA, 1);
+        EasyMock.expectLastCall().times(2);
+        mockMap.addArmies(TerritoryName.ALBERTA, 1);
+        EasyMock.expectLastCall().times(2);
+        EasyMock.replay(mockMap);
+        game.provideWorldMap(mockMap);
+        game.setPhase(GamePhase.FORTIFY);
+        game.setCurrentPlayer(PlayerColor.RED);
+        game.fortify(TerritoryName.ALASKA, TerritoryName.ALBERTA, 1);
+        game.endTurn();
+        game.setPhase(GamePhase.FORTIFY);
+        game.setCurrentPlayer(PlayerColor.BLUE);
+        game.fortify(TerritoryName.ALASKA, TerritoryName.ALBERTA, 1);
+        EasyMock.verify(mockMap);
+    }
 }
