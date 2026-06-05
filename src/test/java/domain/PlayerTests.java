bc2a668695226af1a0b7887ce10a4ced9ba1fcc4
Jonathan Fang <jonathanfang2026@u.northwestern.edu>
2026-06-05 13:04:18 -0500

    RemoveCards_PlayerHasThreeCards_RemoveOne_PlayerHasTwoCards passes

--- src/test/java/domain/PlayerTests.java
+++ src/test/java/domain/PlayerTests.java
@@ -250,4 +250,17 @@ public class PlayerTests {
         player.removeCards(List.of(alaska));
         assertEquals(0, player.getCardCount());
     }
+
+    @Test
+    public void RemoveCards_PlayerHasThreeCards_RemoveOne_PlayerHasTwoCards() {
+        Player player = new Player(PlayerColor.RED, "Justin", 35);
+        Card alaska = new Card(CardType.INFANTRY, TerritoryName.ALASKA);
+        Card alberta = new Card(CardType.CAVALRY, TerritoryName.ALBERTA);
+        Card brazil = new Card(CardType.ARTILLERY, TerritoryName.BRAZIL);
+        player.addCard(alaska);
+        player.addCard(alberta);
+        player.addCard(brazil);
+        player.removeCards(List.of(alaska));
+        assertEquals(List.of(alberta, brazil), player.getCards());
+    }
 }
