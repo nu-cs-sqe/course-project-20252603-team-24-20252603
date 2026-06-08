aa4558a95531ac4b334e34f525ef3bab3ce724e7
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2026-06-07 22:59:12 -0500

    Launch app through GuiSceneNavigation on startup.

--- src/main/java/gui/RiskApplication.java
+++ src/main/java/gui/RiskApplication.java
@@ -1,9 +1,6 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -13,18 +10,7 @@ public final class RiskApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                RiskApplication.class.getResource("/game-setup-view.fxml"),
-                LocaleManager.getBundle());
-
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        stage.setTitle(LocaleManager.getBundle().getString("window.title.setup"));
-        stage.setWidth(520);
-        stage.setHeight(420);
-        stage.setMinWidth(480);
-        stage.setMinHeight(360);
-        stage.setScene(scene);
+        GuiSceneNavigation.showSetupScreen(stage);
         stage.show();
     }
 
